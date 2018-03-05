3d79c6589300895e242ffec34719d1129a98d08e
brandojc <brandojc@rose-hulman.edu>
2018-03-05 09:15:08 -0500

    Completed

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -10,11 +10,11 @@ Also:
   -- ASSIGNING a VALUE to a NAME (VARIABLE).
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and James Brandon.
 """
 ###############################################################################
 #
-# TODO: 1.
+# Done: 1.
 #   Yes, that means for YOU to DO things per the following instructions:
 #
 #   On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 2.
+# Done: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -43,13 +43,13 @@ import rosegraphics as rg
 #   in all projects that use rosegraphics, so get used to it. :)
 #
 #   Once  rosegraphics  in the  import  statement is no longer marked
-#   as an error, change the above TO-DO to DONE and coninue to the next TO-DO.
+#   as an error, change the above TO-DO to DONE and continue to the next TO-DO.
 #
 ###############################################################################
 
 ###############################################################################
 #
-# TODO: 3.
+# Done: 3.
 #   Run this module.  A window will pop up and Turtles will move around.
 #   After the Turtles stop moving, *click anywhere in the window to close it*.
 #
@@ -101,11 +101,19 @@ natasha.speed = 10  # Faster
 natasha.backward(50)
 natasha.right(90)
 natasha.forward(50)
+natasha.pen = rg.Pen('blue',15)
+natasha.speed = 15
+natasha.left(45)
+natasha.forward(30)
 
-
+jamarcus = rg.SimpleTurtle()
+jamarcus.pen = rg.Pen('purple',15)
+jamarcus.forward(50)
+jamarcus.right(90)
+jamarcus.forward(300)
 ###############################################################################
 #
-# TODO: 4.
+# Done: 4.
 #   Add a few more line of your own code to make one of the
 #   existing SimpleTurtles move some more and/or have different
 #   characteristics.
@@ -119,7 +127,7 @@ natasha.forward(50)
 
 ###############################################################################
 #
-# TODO: 5.
+# Done: 5.
 #   The above code  CONSTRUCTS  two SimpleTurtle objects
 #   and gives those objects NAMES:
 #       dave    matt
